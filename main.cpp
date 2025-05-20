d8aeaba50ac66397d8e1749464676c1c4fe74be0
kmbrodzicki <kmbrodzicki@student.agh.edu.pl>
2025-05-20 15:20:00 +0200

    Final commit without /x64 folder

--- main.cpp
+++ main.cpp
@@ -13,14 +13,11 @@ using namespace GameEngine;
 int main() {
     Boss* boss = new Boss("MadDog", 150, 25, 10, SWORDSMAN, 8);
     UI* uiObserver = new UI();
-    Enemy* wrog = new Enemy("Szat", 100, 10, 10, BEAST);
 
     boss->addObserver(uiObserver);  
     boss->getHit(30);               
     boss->heal(20);     
-    boss->attack(*wrog);
-    boss->die();
-
+    
     delete boss;
     delete uiObserver;
 
